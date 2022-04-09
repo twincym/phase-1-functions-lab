4b9f7ff6c16737759ef2185df0a1b58ed638f956
twincy modi <twincymodi@gmail.com>
2022-04-09 12:20:05 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(number){
+    return Math.abs(42-number)
+}
+
+function distanceFromHqInFeet(number){
+    return distanceFromHqInBlocks(number)*264
+}
+
+function distanceTravelledInFeet(number1, number2){
+    return (Math.abs(number2-number1)*264)
+}
+
+function calculatesFarePrice(start, destination){
+    let finalDestinaion = (Math.abs(start-destination)*264)
+    if (finalDestinaion <401){
+        return 0
+    }
+    else if (finalDestinaion < 2000){
+        return ((finalDestinaion-400) * 0.02)
+    }
+    else if (finalDestinaion < 2500){
+        return 25
+    }
+    else if (finalDestinaion > 2500){
+        return "cannot travel that far"
+    }}
